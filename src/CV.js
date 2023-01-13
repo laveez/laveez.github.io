cad09fc7ba8cdfa826d3487213e1b0b2ac57dbe2
Laveez <niko.muukkonen@gofore.com>
2023-01-13 17:34:46 +0200

    V2 Improvements

--- src/CV.js
+++ src/CV.js
@@ -1,8 +1,94 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import resume from './resume.json';
 import './App.css';
 
+const CVSection = ({ title, items, className }) => {
+  if (!items) {
+    return null;
+  }
+  return (
+    <section>
+      <h1>{title}</h1>
+      {items.map((item,index) => (
+        <div key={index} className={`${title.toLowerCase()}-item`}>
+          <h3>{item.startDate} - {item.endDate}</h3>
+          <h2>{item.name || item.organization || item.institution}</h2>
+          <h3>{item.position || item.studyType}{item.area && (item.position || item.studyType) && ','} {item.area} </h3>
+          {item.summary && <p>{item.summary}</p>}
+        </div>
+      ))}
+    </section>
+  )
+}
+
 const CV = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 1480) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <>
+      {isMobile ? (
+        <MobileCV />
+      ) : (
+        <DesktopCV />
+      )}
+    </>
+  );
+}
+
+const MobileCV = () => {
+
+  return (
+    <div className="mobile-section">
+      <section className="mobile-basics">
+        <img src={resume.basics.image} alt={resume.basics.name} className="avatar"/>
+        <h1>{resume.basics.name}</h1>
+        <h2>{resume.basics.label}</h2>
+        <a href={resume.basics.url}>{resume.basics.url}</a>
+        <p>{resume.basics.summary}</p>
+      </section>
+      <CVSection title="Education" items={resume.education} />
+      <CVSection title="Experience" items={resume.work} />
+      <CVSection title="Volunteer" items={resume.volunteer} />
+      <section>
+        <h2>Certificates</h2>
+        <ul>
+          {resume.certificates.map((cert, index) => (
+            <li key={index}>{cert.name}, {cert.issuer}, {cert.date}</li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h2>Skills</h2>
+        <div className="skills-grid">
+          {resume.skills.map((skill, index) => (
+            <div key={index} className="skill">
+              {skill.name}
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+const DesktopCV = () => {
+  if (!resume) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className="container">
       <div className="left-column">
@@ -15,38 +101,14 @@ const CV = () => {
         </section>
       </div>
       <div className="right-column">
-        <section>
-          <h2>Education</h2>
-          {resume.education.map((edu) => (
-            <div className="education-item">
-              <h3>{edu.studyType} in {edu.area}, {edu.institution}, {edu.startDate} - {edu.endDate}</h3>
-              <p>{edu.description}</p>
-            </div>
-          ))}
-        </section>
-        <section>
-          <h2>Experience</h2>
-          {resume.work.map((work) => (
-            <div className="experience-item">
-              <h3>{work.position}, {work.name}, {work.startDate} - {work.endDate}</h3>
-              <p>{work.summary}</p>
-            </div>
-          ))}
-        </section>
-        <section>
-          <h2>Volunteer</h2>
-          {resume.volunteer.map((volunteer) => (
-            <div className="volunteer-item">
-              <h3>{volunteer.position}, {volunteer.organization}, {volunteer.startDate} - {volunteer.endDate}</h3>
-              <p>{volunteer.summary}</p>
-            </div>
-          ))}
-        </section>
+        <CVSection title="Education" items={resume.education} />
+        <CVSection title="Experience" items={resume.work} />
+        <CVSection title="Volunteer" items={resume.volunteer} />
         <section>
           <h2>Certificates</h2>
           <ul>
-            {resume.certificates.map((cert) => (
-              <li>{cert.name}, {cert.issuer}, {cert.date}</li>
+            {resume.certificates.map((cert, index) => (
+              <li key={index}>{cert.name}, {cert.issuer}, {cert.date}</li>
             ))}
           </ul>
         </section>
@@ -61,8 +123,7 @@ const CV = () => {
           </div>
         </section>
       </div>
-    </div>
-  );
+    </div>);
 }
 
 export default CV;
